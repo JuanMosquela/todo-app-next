94aeffc069ead1403174e7e26118b8129eece1ed
JuanMosquella <94761627+JuanMosquela@users.noreply.github.com>
2023-08-28 13:44:42 -0300

    moved revalidate path

--- app/create-todo/page.tsx
+++ app/create-todo/page.tsx
@@ -7,23 +7,24 @@ import { revalidatePath } from "next/cache";
 async function createTodo(data: FormData) {
   "use server";
   connectToDB();
-  revalidatePath("/");
   const title = data.get("title")?.valueOf();
   if (typeof title !== "string" || title.length === 0) {
     throw new Error("Invalid Title");
   }
   await new Todo({ title }).save();
+  revalidatePath("/");
   redirect("/");
 }
 
 export default function Page() {
   return (
-    <>
+    <section className="px-20">
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl text-white">New</h1>
       </header>
       <form action={createTodo} className="flex gap-2 flex-col">
         <input
+          autoFocus={true}
           type="text"
           name="title"
           className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100 text-white"
@@ -43,6 +44,6 @@ export default function Page() {
           </button>
         </div>
       </form>
-    </>
+    </section>
   );
 }
